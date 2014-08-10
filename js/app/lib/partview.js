cdb388ff8aae02fa04e57ac8b1236d4b4dd4eae6
Daniel Joyce <daniel.a.joyce@gmail.com>
2014-08-09 21:47:40 -0700

    Got transforming working, refactored picking. Need to tighten up state flow in MainView

--- js/app/lib/partview.js
+++ js/app/lib/partview.js
@@ -38,16 +38,7 @@ define(['jquery', 'applib/hardpoint', 'applib/common', 'applib/scene'], function
 		if (this._pickEnabled === true && this._mouseMoved === false && event.button === 0) {
 			this._pickWidget.visible = true;
 			event.preventDefault();
-			var domElement = this._renderer.domElement;
-			var mouse = new THREE.Vector2();
-			var pos = $(domElement).position();
-			var relX = event.pageX - pos.left;
-			var relY = event.pageY - pos.top;
-			var mouseVector = new THREE.Vector3((relX / domElement.width ) * 2 - 1, -(relY / domElement.height ) * 2 + 1, 0);
-			// Fixup mouse vector relative to camera.
-			this._projector.unprojectVector(mouseVector, this._camera);
-			this._raycaster.set(this._camera.position, mouseVector.sub(this._camera.position).normalize());
-			var picked = this._raycaster.intersectObjects(this._pickableObjects, true);
+			var picked = this._doPick([this._rootModel], true);
 			// console.debug(picked);
 			if (picked.length > 0) {
 				console.debug("HIT!");
